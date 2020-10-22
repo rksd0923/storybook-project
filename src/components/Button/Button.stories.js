4eaefe09b474789e7ff196b39dc34f725c026adc
Ruben Rivera <rerivera76@gmail.com>
2020-10-22 11:58:06 -0400

    test

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,6 +2,67 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import Button from './Button';
+/*const style = {
+    backgroundColor: '#0069D9',
+    color: 'white',
+    borderRadius: '2px',
+    padding: '5px 25px',
+    border: '#0069D9'
+}
+
+const danger = {
+    backgroundColor: '#0069D9',
+    color: 'white',
+    borderRadius: '2px',
+    padding: '5px 25px',
+    border: '#0069D9'
+}
+
+const warning = {
+    backgroundColor: '#0069D9',
+    color: 'white',
+    borderRadius: '2px',
+    padding: '5px 25px',
+    border: '#0069D9'
+}
+
+const success = {
+    backgroundColor: '#0069D9',
+    color: 'white',
+    borderRadius: '2px',
+    padding: '5px 25px',
+    border: '#0069D9'
+}
+*/
+
 
 storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+    .add('Primary', () => <Button 
+        label="Primary" 
+        type="primary"
+        />) 
+        .add('Large Primary', () => <Button 
+        label="Large Primary Button" 
+        type="primary"
+        />) 
+
+        .add('Danger', () => <Button 
+        label="Danger" 
+        type="danger"
+        />) 
+        .add('Large Danger', () => <Button 
+        label="Large Danger Button" 
+        type="danger"
+        />) 
+
+        .add('Warning', () => <Button 
+        label="Warning" 
+        type="warning"
+        />) 
+
+        .add('Success', () => <Button 
+        label="Success" 
+        type="success"
+        />) 
+
+        
